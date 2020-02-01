8587450412b4e783c21014f8cbfa05b283eea94c
Tarif M <tarif.m@zohocorp.com>
2020-02-01 11:33:05 +0530

    [Enhancement] Adding UserAuthentication

--- src/main/java/com/appsdeveloperblog/app/ws/service/UsersService.java
+++ src/main/java/com/appsdeveloperblog/app/ws/service/UsersService.java
@@ -4,4 +4,8 @@ import com.appsdeveloperblog.app.ws.shared.dto.UserDTO;
 
 public interface UsersService {
 	public UserDTO createUser(UserDTO user);
+
+	public UserDTO getUser(String userId);
+
+	public UserDTO getUserByUserName(String userName);
 }
--- src/main/java/com/appsdeveloperblog/app/ws/ui/entrypoints/UsersEnryPoint.java
+++ src/main/java/com/appsdeveloperblog/app/ws/ui/entrypoints/UsersEnryPoint.java
@@ -4,6 +4,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
@@ -36,9 +37,19 @@ public class UsersEnryPoint {
 
 		return response;
 	}
+
 	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	public String testingJersy() {
-		return "Workin hard";
+	@Path("/{id}")
+	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+	public UserProfileResponse testingJersy(@PathParam("id") String userId) {
+		UserProfileResponse returnValue = null;
+		
+		UsersService userService = new UsersServiceImpl();
+		UserDTO userDto = userService.getUser(userId);
+		
+		returnValue = new UserProfileResponse();
+		BeanUtils.copyProperties(userDto, returnValue);
+		
+		return returnValue;
 	}
 }
